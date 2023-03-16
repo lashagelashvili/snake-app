0a261cbcc54b1e051437c0bcb4bdb77071999037
lashagelashvili <las.gelashvili@gmail.com>
2023-03-17 01:03:04 +0400

    create inital state of snake

--- src/app/components/snake/snake.component.ts
+++ src/app/components/snake/snake.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Snake } from './model';
 
 @Component({
   selector: 'app-snake',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SnakeComponent implements OnInit {
   size = 20;
-  grid = new Array(this.size * this.size);
+  grid: Snake[] = new Array(this.size * this.size)
+    .fill({} as Snake)
+    .map(() => ({ isSnake: false, isFood: false, isHead: false }));
 
   ngOnInit(): void {
+    this.initialState();
     console.log(this.grid);
   }
+
+  initialState() {
+    for (let i = 0; i < 3; i++) {
+      this.grid[i].isSnake = true;
+      console.log('a');
+    }
+  }
+
+  startGame() {}
 }
