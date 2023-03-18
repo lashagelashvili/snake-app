e9f7b47b6dbe315c6a9783cfda8cfb3ec4ff9472
lashagelashvili <las.gelashvili@gmail.com>
2023-03-18 15:34:18 +0400

    update grid to two dimensional array

--- src/app/components/snake/snake.component.ts
+++ src/app/components/snake/snake.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { interval, tap } from 'rxjs';
 import { Snake } from './model';
 
 @Component({
@@ -8,21 +9,72 @@ import { Snake } from './model';
 })
 export class SnakeComponent implements OnInit {
   size = 20;
-  grid: Snake[] = new Array(this.size * this.size)
-    .fill({} as Snake)
-    .map(() => ({ isSnake: false, isFood: false, isHead: false }));
+  grid: Snake[][] = [];
+  // grid: Snake[][] = new Array(this.size * this.size)
+  //   .fill({} as Snake)
+  //   .map(() => ({ isSnake: false, isFood: false, isHead: false }));
+
+  constructor() {}
 
   ngOnInit(): void {
-    this.initialState();
-    console.log(this.grid);
+    this.createMap();
+    // this.initialState();
+    // console.log(this.grid);
+    // interval(700).subscribe((num) => {
+    //   console.log(num);
+    //   let lastCell = this.grid[num];
+    //   let currentHead = this.grid[num + 2];
+    //   let nextCell = this.grid[num + 3];
+    //   // currentHead.isHead = true;
+    //   this.grid[num + 3] = { ...this.grid[num], isHead: true };
+    //   this.grid[num + 2].isHead = false;
+    //   this.grid[num].isSnake = false;
+    //   this.grid[num].isHead = false;
+    //   // nextCell = { ...lastCell, isHead: true };
+    //   // lastCell.isSnake = false;
+    //   // currentHead.isHead = false;
+    // });
   }
 
-  initialState() {
-    for (let i = 0; i < 3; i++) {
-      this.grid[i].isSnake = true;
-      console.log('a');
+  createMap() {
+    for (let i = 0; i < this.size; i++) {
+      this.grid[i] = [];
+      for (let j = 0; j < this.size; j++) {
+        this.grid[i][j] = { isSnake: false, isHead: false, isFood: false };
+      }
     }
   }
 
-  startGame() {}
+  // initialState() {
+  //   for (let i = 0; i < 3; i++) {
+  //     if (i === 2) {
+  //       this.grid[i].isHead = true;
+  //       this.grid[i].isSnake = true;
+  //       return;
+  //     }
+
+  //     this.grid[i].isSnake = true;
+  //   }
+  // }
+
+  // public trackItem(index: number, item: any) {
+  //   return item.isHead;
+  // }
 }
+
+// this.grid = [
+//   [
+//     { isSnake: true, isHead: false, isFood: false },
+//     { isSnake: true, isHead: false, isFood: false },
+//     { isSnake: true, isHead: false, isFood: false },
+//     { isSnake: true, isHead: false, isFood: false },
+//     { isSnake: true, isHead: false, isFood: false },
+//     { isSnake: false, isHead: false, isFood: false },
+//     { isSnake: false, isHead: false, isFood: false },
+//     { isSnake: false, isHead: false, isFood: false },
+//   ],
+//   [],
+//   [],
+//   [],
+//   [],
+// ];
