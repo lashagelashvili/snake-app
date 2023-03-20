ae154a8cff547f479fef849c34500cae0341f20b
lashagelashvili <las.gelashvili@gmail.com>
2023-03-20 16:36:33 +0400

    add models

--- src/app/components/snake/model.ts
+++ src/app/components/snake/model.ts
@@ -1,5 +1,19 @@
 export interface Snake {
+  body: SnakePart[];
+  head: SnakePart;
+}
+
+export interface Map {
+  grid: Tile[][];
+}
+
+export interface Tile {
   isFood: boolean;
   isHead: boolean;
   isSnake: boolean;
+}
+
+export interface SnakePart {
+  i: number;
+  j: number;
 }
