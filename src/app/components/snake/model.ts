0bcd1716d1fea8aab84f80a8290ae6e318709e9d
lashagelashvili <las.gelashvili@gmail.com>
2023-03-20 16:36:58 +0400

    add direction to snake model

--- src/app/components/snake/model.ts
+++ src/app/components/snake/model.ts
@@ -1,6 +1,14 @@
 export interface Snake {
   body: SnakePart[];
   head: SnakePart;
+  direction: Direction;
+}
+
+export enum Direction {
+  UP = 'ArrowUp',
+  RIGHT = 'ArrowRight',
+  DOWN = 'ArrowDown',
+  LEFT = 'ArrowLeft',
 }
 
 export interface Map {
